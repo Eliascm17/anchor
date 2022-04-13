3bbe5dfacf50ffceccb23e0cf653b4916e39a2b9
Eliascm17 <Elias.cmoreno17@gmail.com>
2022-04-13 13:03:25 -0500

    pls work

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-solana_ver=$(cat ./.github/releases/solana.txt)
-source patch_crates.sh
+solana_ver=$(cat ./.github/releases/solana-latest.txt)
+echo $solana_ver
+echo $PWD
+source ./.github/scripts/patch_crates.sh
 
 update_solana_dependencies . $solana_ver
