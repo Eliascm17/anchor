bceeab06ba20022b41fc8b59bd6fa78bf89d2d14
Eliascm17 <Elias.cmoreno17@gmail.com>
2022-04-13 14:22:41 -0500

    asdf

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -2,8 +2,6 @@
 
 solana_ver=$(cat ./.github/releases/solana-latest.txt)
 echo $solana_ver
-echo $PWD
-echo "$(ls -l)"
 source ./.github/scripts/patch_crates.sh
 
-update_solana_dependencies ./ $solana_ver
+update_solana_dependencies . $solana_ver
